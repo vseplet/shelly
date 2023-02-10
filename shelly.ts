182b1e521e2953688df366509adf0ce6d55fe3a4
Artem <artp80@gmail.com>
2023-02-10 21:22:03 +0600

    fix: setTimeout kill after process ends

--- shelly.ts
+++ shelly.ts
@@ -1,4 +1,4 @@
-import { sleep } from "https://deno.land/x/sleep/mod.ts";
+import { sleep } from 'https://deno.land/x/sleep/mod.ts';
 
 interface IShellResponse {
   data: {
@@ -7,50 +7,73 @@ interface IShellResponse {
   };
   error: Error | null;
 }
-async function shelly(command: string, timeout = 4): Promise<IShellResponse> {
-  try {
-    const splitted = command.split(" ");
 
-    const p = Deno.run({ cmd: splitted, stdout: "piped", stderr: "piped" });
-    const processFinite = await Promise.any([
-      p.status(),
-      sleep(timeout),
+interface IShellyOptions {
+  timeout: number;
+}
+
+/**
+ * Выполняет @command, если выполнение займет дольше @timeout, вернет Error.
+ * В случае успеха вернет объект с stdout и stderr
+ * @param command Строка с командой
+ * @param timeout Время в секундах
+ * @returns Объект с stdout и stderr
+ */
+async function shelly(
+  command: string,
+  options: IShellyOptions = { timeout: 4 },
+): Promise<IShellResponse> {
+  try {
+    const splitted = command.split(' ');
+    const proc = Deno.run({ cmd: splitted, stdout: 'piped', stderr: 'piped' });
+    let timeoutId = 0;
+    const sleeper = new Promise((resolve) =>
+      timeoutId = setTimeout(resolve, options.timeout * 1000)
+    );
+    //TODO(artpani) вынести механизм контроля длительности промиса в отдельную функию
+
+    const finiteProc: Deno.ProcessStatus | unknown = await Promise.any([
+      proc.status(),
+      sleeper,
     ]);
-    if (processFinite) {
-      const [{ code }, out, err] = await Promise.all([
-        processFinite as Deno.ProcessStatus,
-        p.output(),
-        p.stderrOutput(),
-      ]);
-      if (code === 0) {
+
+    if (finiteProc as Deno.ProcessStatus) {
+      clearTimeout(timeoutId);
+      const out = new TextDecoder().decode(await proc.output());
+      const err = new TextDecoder().decode(await proc.stderrOutput());
+      const { success, code } = finiteProc as Deno.ProcessStatus;
+      if (success) {
         return {
-          data: { stdout: new TextDecoder().decode(out), stderr: "" },
+          data: { stdout: out, stderr: '' },
           error: null,
         };
       } else {
         return {
-          data: { stdout: "", stderr: new TextDecoder().decode(err) },
-          error: null,
+          data: { stdout: out, stderr: err },
+          error: new Error(JSON.stringify(finiteProc)),
         };
       }
     } else {
-      p.kill();
-      const out = new TextDecoder().decode(await p.output());
-      const err = new TextDecoder().decode(await p.stderrOutput());
-      p.close();
+      proc.kill();
+      proc.close();
+      const out = new TextDecoder().decode(await proc.output());
+      const err = new TextDecoder().decode(await proc.stderrOutput());
       return {
         data: { stdout: out, stderr: err },
-        error: new Error("Процесс занятулся"),
+        error: new Error('Процесс занятулся'),
       };
     }
   } catch (e) {
-    return { data: { stdout: "", stderr: "" }, error: (e as Error) };
+    return { data: { stdout: '', stderr: '' }, error: (e as Error) };
   }
 }
 
 {
-  const { data, error } = await shelly("./sleepAndEcho.sh", 5);
-  console.log(data);
-  console.log(error);
-  console.log("--------------------------------");
+  const { data, error } = await shelly(`./sleepAndEcho.sh`, {
+    timeout: 3,
+  });
+
+  // console.log(data);
+  // console.log(error);
+  // console.log('--------------------------------');
 }
