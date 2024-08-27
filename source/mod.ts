a935f5819b9290a076221ed3f1455a783f6fb49b
Vsevolod Pletnev <Vsevolod.Pletnev@farcana.com>
2024-08-28 01:26:09 +0700

    upd readme

--- source/mod.ts
+++ source/mod.ts
@@ -2,5 +2,4 @@ import { shelly } from "./shelly.ts";
 import { sh } from "./programs/shell.ts";
 import { bash } from "./programs/shell.ts";
 import { zsh } from "./programs/shell.ts";
-
 export { bash, sh, shelly, zsh };
